73caa9e57b4a99e4b504b1daa64c9552346e5e0c
JJacky-hub <jekaolshanskiy@gmail.com>
2026-09-14 13:48:18 +0200

    Update parse_logs.sh

--- parse_logs.sh
+++ parse_logs.sh
@@ -3,9 +3,9 @@
 LOG_FILE="/var/log/auth.log"
 REPORT_FILE="blacklist.txt"
 
-echo "[*] Анализирую логи и собираю IP взломщиков..."
+echo "[*] Analyzing logs..."
 
-# Вытаскиваем IP-адреса из неудачных попыток входа
+
 sudo grep "Failed password" $LOG_FILE | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -u > $REPORT_FILE
 
-echo "[+] Готово! Список IP сохранен в файл $REPORT_FILE"
+echo "[+] Done! IP list saved to $REPORT_FILE"
